1ba29cfc7bfc7381128e0682831d61b6817e8bc7
SistemApp <147223054+TickeVirtual@users.noreply.github.com>
2025-06-10 14:25:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -201,8 +201,8 @@
 
         sendMessageButton.disabled = true; // Deshabilitar botón mientras se envía
 
-        const url = "https://mensajero-evolution-api.ykf6ye.easypanel.host/message/sendMedia/botinstancia"; // Cambia NOMBRE_INSTANCIA
-        const apikey = "9E3F31BC6102-4E56-BA66-2B3F3637A9D7"; // Coloca aquí tu API key
+        const url = "https://mensajero-evolution-api.ykf6ye.easypanel.host/message/sendMedia/laundrydryinstancia"; // Cambia NOMBRE_INSTANCIA
+        const apikey = "108EFEC34CD4-4789-888E-AF00903BAFA4"; // Coloca aquí tu API key
         const numeroTelefono = `+51${telefono}`; // Coloca el número de teléfono del destinatario
         const longURL = window.location.href; // Obtiene la URL actual
 
